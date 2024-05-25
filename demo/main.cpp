2147112dc0f76f4e889321389323de0729cd5075
KingsFourze <kingsfourze@rc9453.com>
2024-05-25 09:02:45 +0800

    update demo for deny close endpoint

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,12 +8,16 @@ using namespace SafetyTcpConn;
 int main(int, char**) {
 
     Endpoint endpoint(8080,
-        [](Endpoint* endpoint, ConnectionPtr conn) {
-            std::stringstream ss("");
-            char buff[1024];
+        [](ConnectionPtr conn) {
+            std::cout << "SafetyTcpConn >> Endpoint >> Client Connected | FD:" << conn->m_fd_ << std::endl;
+
+        },
+        [](ConnectionPtr conn) {
+            std::cout << "SafetyTcpConn >> Endpoint >> Message Come | FD:" << conn->m_fd_ << std::endl;
 
             // recv full msg, msg is end with \r\n
-            std::string fullMsg = ss.str();
+            std::string fullMsg = "";
+            char buff[1024];
             while (fullMsg.size() < 2 || fullMsg.substr(fullMsg.size() - 2, 2) != "\r\n") {
                 size_t readSize = conn->ReadString(buff, 1024);
                 if (readSize == 0) {
@@ -22,18 +26,20 @@ int main(int, char**) {
                     return;
                 }
 
-                // push string into stringstream
-                ss << std::string(buff, buff + readSize);
-                fullMsg = ss.str();
+                // concat readed data into fullMsg
+                fullMsg.append(buff, buff + readSize);
             }
 
-            std::cout << ss.str();
-            conn->MsgEnqueue(ss.str().c_str(), ss.str().size());
+            std::cout << fullMsg;
+            conn->MsgEnqueue(fullMsg.c_str(), fullMsg.size());
         },
-        [](Endpoint* endpoint, ConnectionPtr conn) {
-
+        [](ConnectionPtr conn) {
+            std::cout << "SafetyTcpConn >> Endpoint >> Client Disconnected | FD:" << conn->m_fd_ << std::endl;
         }
     );
+
+    // while loop to keep endpoint running
+    while(true) usleep(1000);
 
     return 0;
 }
